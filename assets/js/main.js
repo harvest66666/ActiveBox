035d786ccc01b301488c1b5a057ab1e7476bba66
harvest66666 <harvest66666666@gmail.com>
2022-05-31 21:17:52 +0300

    js commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
 	let header = $("#header");
 	let intro = $("#intro");
 	let introHeight;
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
 
 	$(window).on("scroll load resize", function () {
 		let introHeight = intro.innerHeight();
@@ -21,9 +24,33 @@ $(document).ready(function () {
 		let elementId = $(this).data("scroll");
 		let elementOffset = $(elementId).offset().top;
 
+		nav.removeClass("show");
+
 		$("html, body").animate({
-			scrollTop: elementOffset - 100
+			scrollTop: elementOffset - 70
 		});
 
+	});
+
+
+	// Nav Toggle
+
+	navToggle.on("click", function (event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
+	});
+
+
+	// slick slider
+	let slider = $("#clients-Slider");
+
+	slider.slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		arrows: false,
+		dots: true
 	});
 });
